66fc567cfe82b30228c7de2a15daf6798e30697f
Hyacinmond <3100352303@qq.com>
2025-01-19 15:35:03 +0800

    📝

---
+++ PS_AMP_TEST/PS_AMP_TEST.sdk/CPU1/src/main.c
@@ -0,0 +1,36 @@
+#include "xparameters.h"
+#include "xgpiops.h"
+#include "xstatus.h"
+#include "xplatform_info.h"
+#include "sleep.h"
+
+#define LED2 55
+
+#define GPIO_DEVICE_ID XPAR_XGPIOPS_0_DEVICE_ID
+XGpioPs Gpio;
+
+void Gpio_Init(void)
+{
+    XGpioPs_Config *ConfigPtr;
+
+    ConfigPtr = XGpioPs_LookupConfig(GPIO_DEVICE_ID);
+    XGpioPs_CfgInitialize(&Gpio, ConfigPtr, ConfigPtr->BaseAddr);
+
+    XGpioPs_SetDirectionPin(&Gpio, LED2, 1);
+    XGpioPs_SetOutputEnablePin(&Gpio, LED2, 1);
+    XGpioPs_WritePin(&Gpio, LED2, 0);
+}
+
+int main(void)
+{
+    Gpio_Init();
+    while (1)
+    {
+        XGpioPs_WritePin(&Gpio, LED2, 0);
+        sleep(2);
+        XGpioPs_WritePin(&Gpio, LED2, 1);
+        sleep(2);
+    };
+
+    return 0;
+}
---
+++ PS_AMP_TEST/PS_AMP_TEST.sdk/ZYNQ_CORE_wrapper/platform.tcl
@@ -0,0 +1,38 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct D:\Programs\Workspace\SmartZYNQ_SP2\PS_AMP_TEST\PS_AMP_TEST.sdk\ZYNQ_CORE_wrapper\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source D:\Programs\Workspace\SmartZYNQ_SP2\PS_AMP_TEST\PS_AMP_TEST.sdk\ZYNQ_CORE_wrapper\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {ZYNQ_CORE_wrapper}\
+-hw {D:\Programs\Workspace\SmartZYNQ_SP2\PS_AMP_TEST\ZYNQ_CORE_wrapper.xsa}\
+-out {D:/Programs/Workspace/SmartZYNQ_SP2/PS_AMP_TEST/PS_AMP_TEST.sdk}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {empty_application}
+platform generate -domains 
+platform active {ZYNQ_CORE_wrapper}
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+platform generate -quick
+platform active {ZYNQ_CORE_wrapper}
+domain create -name {standalone_ps7_cortexa9_1} -display-name {standalone_ps7_cortexa9_1} -os {standalone} -proc {ps7_cortexa9_1} -runtime {cpp} -arch {32-bit} -support-app {empty_application}
+platform generate -domains 
+platform write
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+domain active {standalone_ps7_cortexa9_1}
+platform generate -quick
+bsp reload
+domain active {standalone_ps7_cortexa9_0}
+bsp reload
+domain active {standalone_ps7_cortexa9_1}
+bsp config extra_compiler_flags "-mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -nostartfiles -g -Wall -Wextra -fno-tree-loop-distribute-patterns -DUSE_AMP=1"
+bsp config extra_compiler_flags "-mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -nostartfiles -g -Wall -Wextra -fno-tree-loop-distribute-patterns -DUSE_AMP=1"
+bsp write
+bsp reload
+catch {bsp regenerate}
